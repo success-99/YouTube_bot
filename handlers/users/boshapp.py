5ac4fa6983a918f9e6a3b713946e678a4b7011f9
MacBook Pro <mbp13@MacBookPro.local>
2023-02-21 18:22:09 +0500

    key

--- handlers/users/boshapp.py
+++ handlers/users/boshapp.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from loader import dp, bot
 from aiogram.dispatcher.filters import Text
-from youtube import youtubemp4
+from youtube import youtubedow
 
 
 
@@ -10,18 +10,14 @@ async def boshla(message: types.Message):
     await message.answer('YouTube havolasini tashlang')
 
 
-# @dp.message_handler()
-# async def cmd_answer(message: types.Message):
-#     if message.text.startswith('https://fb') \
-#             or message.text.startswith('https://www.facebook.com'):
 @dp.message_handler(Text(startswith="https://", ))
 async def send_link(message: types.Message):
         link = message.text
-        data5 = youtubemp4(link)
+        data5,yy = youtubedow(link)
         if data5 == "bed":
             await message.answer("Hechnarsa topilmadi 😔")
         else:
-            await message.delete()
+
             xabar = await bot.send_message(chat_id=message.chat.id, text="kuting")
             for i in range(1, 11):
                 text0 = i * 10
@@ -29,5 +25,7 @@ async def send_link(message: types.Message):
                 text2 = (10 - i) * "▫️️"
                 await xabar.edit_text(f"{text0}%\n{text1}{text2}")
             await xabar.delete()
+
             await message.answer_video(data5)
+            await message.answer(yy)
 
